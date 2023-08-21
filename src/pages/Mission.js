63179a3998f769a595ae4e0a522284eefe877ee5
kimgogowow <kimwu9419@gmail.com>
2023-08-21 11:56:04 -0400

    updated: mission page

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -5,6 +5,17 @@ export default function Mission() {
         <>
             <div id="mission-container">
 
+                <img src="mission.png" alt="mission path" />
+                <div id="mission_text">
+                    Local Asian to non-Asian connections via language learning. There are over 1 M international
+                    students from Asia that study in the US each year. While a majority of Americans embrace
+                    multicultural relationships, many Asian Americans still feel their culture isn’t readily accepted.
+                    Tindoori is a dynamic digital space: users start as individual language learners, find a match in
+                    our API to non-API matchpool, and evolve to learn languages together in chat.
+                    Love found in translation (18+).
+                </div>
+
+
                 <br />
                 <Link to='/future' >
                     <Button text="what is next?" />
